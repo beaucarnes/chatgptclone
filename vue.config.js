8219587f2234f4088c3c50f4ecbf9298e2146e26
Beau Carnes <1513130+beaucarnes@users.noreply.github.com>
2023-08-05 17:00:27 -0400

    updating config

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,16 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  devServer: {
-    host: '0.0.0.0',
-
-
-  }
-})
-
-// module.exports = {
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true,
 //   devServer: {
 //     host: '0.0.0.0',
-//     disableHostCheck: true
+
+
 //   }
-// }
+// })
+
+module.exports = {
+  devServer: {
+    host: '0.0.0.0',
+    // disableHostCheck: true
+  }
+}
